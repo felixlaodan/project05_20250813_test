0c84a6dddb4273cb165b06819cd43b6b0049259b
Felix_laodan <felix.dong.laodan@gmail.com>
2025-08-13 16:38:07 +0800

    修改再提交第二次

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 import chalk from 'chalk';
 
 console.log(chalk.blue('Hello world!'));
+
+// const uniq = require('uniq');
+
+// console.log(uniq([10,20,30,40,30,20,10]));
+
+var arr = [1, 1, 2, 2, 3, 5]
+ 
+require("uniq")(arr)
+console.log(arr)
